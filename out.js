3f57c16eedb0cf6f9a3d8227323b5523ea4126da
kevindu1993 <350511359@qq.com>
2016-03-19 17:14:13 +0800

    useragent

---
+++ out.js
@@ -0,0 +1,18 @@
+{ options: { useFeatures: false, detectCamouflage: true },
+  browser: 
+   { stock: false,
+     hidden: true,
+     channel: '',
+     name: 'Chrome',
+     version: { original: '19.0.1084.60', alias: null, details: 2 } },
+  engine: 
+   { name: 'Webkit',
+     version: { original: '534.46.0', alias: null } },
+  os: { name: 'iOS', version: { original: '5.1.1', alias: null } },
+  device: 
+   { type: 'mobile',
+     identified: true,
+     manufacturer: 'Apple',
+     model: 'IPHONE' },
+  camouflage: false,
+  features: [] }
